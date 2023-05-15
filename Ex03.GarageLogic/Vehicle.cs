2a28def3e24679c76e77e725ae1680ee06a916b4
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-05-15 19:58:42 +0300

    add vehicle class

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Vehicle
+    {
+        private string m_ModelName;
+        private string m_LicensePlate;
+        private float m_RemainingEnergyPercent;
+
+        public Vehicle(string i_ModelName, string i_LicensePlate, float i_RemainingEnergyPercent)
+        {
+            m_ModelName = i_ModelName;
+            m_LicensePlate = i_LicensePlate;
+            m_RemainingEnergyPercent = i_RemainingEnergyPercent;
+        }
+    }
+}
