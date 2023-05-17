e8db7e0d3085cfedd60f3377cc693aeab1dfee20
Yoav Schwartz <yoavsc@mta.ac.il>
2023-05-17 18:19:15 +0300

    Add some initial classes

---
+++ Ex03.GarageLogic/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Motorcycle : Vehicle
+    {
+        private eMotorcycleLicenseType m_LicenseType;
+        private int m_EngineCapacity;
+
+        public Motorcycle(eMotorcycleLicenseType i_LicenseType, int i_EngineCapacity, string i_ModelName, string i_LicensePlate, float i_RemainingEnergyPercent, List<Tire> i_Tires) : base(i_ModelName, i_LicensePlate, i_RemainingEnergyPercent, i_Tires)
+        {
+            m_LicenseType = i_LicenseType;
+            m_EngineCapacity = i_EngineCapacity;
+        }
+    }
+}
